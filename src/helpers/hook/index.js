395efc7d7dd989db7a616becd5b55675e08b1b7d
Wildhoney <adam.timberlake@gmail.com>
2019-10-03 22:43:27 +0100

    Fixed bug in `useForm` hook

--- src/helpers/hook/index.js
+++ src/helpers/hook/index.js
@@ -4,17 +4,16 @@ import _ from 'lodash';
 export default function useForm(initialState) {
     const [state, setState] = useState(initialState);
 
-    const get = () => useCallback(name => _.get(state, name), [state]);
+    const get = useCallback(name => _.get(state, name), [state]);
 
     const set = useCallback((name, value) => {
         const setter = value => setState(state => ({ ..._.set(state, name, value) }));
         return value ? setter(value) : useCallback(setter, []);
     }, []);
 
-    const remove = () =>
-        useCallback(name => setState(state => ({ ..._.unset(state, name) })), [state]);
+    const remove = useCallback(name => setState(state => ({ ..._.unset(state, name) })), [state]);
 
-    const reset = () => useCallback(setState(initialState), []);
+    const reset = useCallback(() =>  setState(initialState), [state]);
 
     return [state, { get, remove, reset, set }];
 }
